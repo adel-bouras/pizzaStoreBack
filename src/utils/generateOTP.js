dfc4f82be5fe772bfbfee2219d49aaea0ba598b0
adel-bouras <eyyad.kareem1212@gmail.com>
2024-09-02 21:12:15 +0100

    done

--- src/utils/generateOTP.js
+++ src/utils/generateOTP.js
@@ -5,7 +5,7 @@ module.exports = (req , res , next)=>{
     const digit = '0123456789';
     let OTP = "";
     for(i=0 ; i<4 ; i++){
-        OTP += digit[Math.floor(Math.random()*11)];
+        OTP += digit[Math.floor(Math.random()*10)];
     }
     
     return OTP
